3667914df161d8d10ad2ad9faa7ce29c12394269
Anafo <AnassTaher2003@hotmail.com>
2024-08-21 21:10:02 +0200

    fast commit

--- flask_app.py
+++ flask_app.py
@@ -5,22 +5,65 @@ import os
 
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import base64
+
 app = Flask(__name__)
+CORS(app)
 
 
-def extract_pdf_data(filename : str):
+
+def extract_pdf_data():
     # Extracting data from PDF
 
-    current_dir = "mysite/"
-    filename = f"{current_dir}{filename}"
+    # check the body of the request
 
-    if not os.path.exists(f"{filename}.pdf"):
-        return json.dumps({
-            "error": "File not found", 
-            "files": os.listdir(current_dir), 
-            "path": current_dir + filename}
-        )
+
+    if request.data:
+        # print("Request data: ", request.data)
+        data = request.get_json()
+        # access the "is_local" key
+        if "pdf_data" in data:
+            # pdf_data is in base64 format, decode it and save it to a file
+            pdf_data = data["pdf_data"]
+
+            # Remove the "data:application/pdf;base64," prefix if it exists
+            if pdf_data.startswith("data:application/pdf;base64,"):
+                pdf_data = pdf_data[len("data:application/pdf;base64,"):]
+
+            # Decode the base64 data
+            decoded_pdf_data = base64.b64decode(pdf_data)
+
+            # Save the decoded data to a PDF file
+            with open("output.pdf", "wb") as pdf_file:
+                pdf_file.write(decoded_pdf_data)
+
+            
+
+
+
+    current_dir = ""
+    # if request.data:
+    #     print("Request data: ", request.data)
+    #     data = request.get_json()
+    #     print("Data: ", data)
+    #     # access the "is_local" key
+    #     if "is_local" in data:
+    #         is_local = data["is_local"]
+    #         if not is_local:
+    #             current_dir = "mysite/"
+    filename = "output"
+    # filename = f"{current_dir}{filename}"
+
+
+    # if not os.path.exists(f"{filename}.pdf"):
+    #     return json.dumps({
+    #         "error": "File not found", 
+    #         "files": os.listdir(current_dir), 
+    #         "path": current_dir + filename}
+    #     )
     
 
     
@@ -82,11 +125,12 @@ def extract_pdf_data(filename : str):
     # with open(f"{filename}.json", "w") as outfile:
     # 	json.dump(dict_info, outfile)
 
-    for i in range(rows):
-        print(grid[i])
+    # for i in range(rows):
+    #     print(grid[i])
 
     # delete the image file
     os.remove(image_file_path)
+    os.remove("output.pdf")
 
     return json.dumps(dict_info)
 
@@ -95,9 +139,9 @@ def extract_pdf_data(filename : str):
 def home():
     return 'windows'
 
-@app.route('/extract/<filename>')
-def extract(filename):
-    return extract_pdf_data(filename)
+@app.route('/extract', methods=['GET', 'POST'])
+def extract():
+    return extract_pdf_data()
 
 if __name__ == '__main__':
     app.run(debug=True)
