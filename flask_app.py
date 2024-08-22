42e5a6c6b62f3bca7f641ab68b0f42d63f55f8af
Anafo <AnassTaher2003@hotmail.com>
2024-08-22 14:53:21 +0200

    fast commit

--- flask_app.py
+++ flask_app.py
@@ -9,17 +9,25 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import base64
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+
+from langchain_community.document_loaders import PyPDFLoader
+
+
 
 app = Flask(__name__)
 CORS(app)
-
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("API_KEY")
+    )
 
 
 def extract_pdf_data():
-    # Extracting data from PDF
-
-    # check the body of the request
-
+    
 
     if request.data:
         # print("Request data: ", request.data)
@@ -29,7 +37,7 @@ def extract_pdf_data():
             # pdf_data is in base64 format, decode it and save it to a file
             pdf_data = data["pdf_data"]
 
-            # Remove the "data:application/pdf;base64," prefix if it exists
+
             if pdf_data.startswith("data:application/pdf;base64,"):
                 pdf_data = pdf_data[len("data:application/pdf;base64,"):]
 
@@ -40,35 +48,10 @@ def extract_pdf_data():
             with open("output.pdf", "wb") as pdf_file:
                 pdf_file.write(decoded_pdf_data)
 
-            
-
-
-
     current_dir = ""
-    # if request.data:
-    #     print("Request data: ", request.data)
-    #     data = request.get_json()
-    #     print("Data: ", data)
-    #     # access the "is_local" key
-    #     if "is_local" in data:
-    #         is_local = data["is_local"]
-    #         if not is_local:
-    #             current_dir = "mysite/"
     filename = "output"
-    # filename = f"{current_dir}{filename}"
 
 
-    # if not os.path.exists(f"{filename}.pdf"):
-    #     return json.dumps({
-    #         "error": "File not found", 
-    #         "files": os.listdir(current_dir), 
-    #         "path": current_dir + filename}
-    #     )
-    
-
-    
-
-    
     pages = None
     try:
         pages = convert_from_path(f"{filename}.pdf", 500)
@@ -77,8 +60,6 @@ def extract_pdf_data():
             "error": str(e),
             "mysite_files" : os.listdir("mysite/"),
             "root_files" : os.listdir("/"),
-            # "files": os.listdir(current_dir),
-            "path": current_dir + filename
         }
 
         return json.dumps(info_obj, indent=4)
@@ -123,19 +104,70 @@ def extract_pdf_data():
         "data" : grid
     }
 
-
-    # with open(f"{filename}.json", "w") as outfile:
-    # 	json.dump(dict_info, outfile)
-
-    # for i in range(rows):
-    #     print(grid[i])
-
-    # delete the image file
+    # delete the image file and pdf file
     os.remove(image_file_path)
     os.remove("output.pdf")
 
     return json.dumps(dict_info)
 
+
+def gpt():
+
+    load_dotenv()
+    if request.data:
+        # print("Request data: ", request.data)
+        data = request.get_json()
+        # access the "is_local" key
+        if "pdf_data" in data:
+            # pdf_data is in base64 format, decode it and save it to a file
+            pdf_data = data["pdf_data"]
+
+
+            if pdf_data.startswith("data:application/pdf;base64,"):
+                pdf_data = pdf_data[len("data:application/pdf;base64,"):]
+
+            # Decode the base64 data
+            decoded_pdf_data = base64.b64decode(pdf_data)
+
+            # Save the decoded data to a PDF file
+            with open("adviesbox.pdf", "wb") as pdf_file:
+                pdf_file.write(decoded_pdf_data)
+
+
+    pdf = "adviesbox.pdf"
+    loader = PyPDFLoader(pdf)
+    documents = loader.load()
+    question = """
+    In het document, is het een huurwoning? Zo ja, wat is de huurprijs per maand. Zo niet, beantwoord deze vragen met behulp van sectie 2.1 in het document: Is de woning bedoeld voor blijven te wonen(primaire woning) of verkoop of verhuur? Is de woning verkocht? Wat is de verwachte verkoopwaarde(marktwaarde)? Wat zijn de verkoopkosten? Wat is de WOZ-waarde? Geef een json object terug met de volgende template:{ "type": string ("eigen", "huur", "inwonend"), "huurprijs" : int, "doel_woning": string, ("wonen", "verkoop", "verhuur") "is_verkocht": Boolean, "verkoopwaarde": int, "verkoopkosten": int, "woz" : int }. Gebruik geen text of iets, het enige wat ik wil krijgen is het json object, zonder apostrophe erbij(`). Dit zal worden gebruiktt in een API request dus ik moet het direct kunnen gebruiken als json object in python
+    """
+
+    # using the pdf's content in documents, and the question, use gpt-4o to answer the question
+
+    # make documents a string
+
+
+    doc_strings = [x.dict()['page_content'] for x in documents]
+    
+    pdf_string = " ".join(doc_strings)
+
+
+
+    answer = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": question + " Here is the content of the pdf: " + pdf_string
+            }
+        ]
+    )
+
+    print(answer.choices[0].message.content)
+    woningsituatie = json.loads(answer.choices[0].message.content)
+    print(woningsituatie)
+
+    return woningsituatie
 
 @app.route('/')
 def home():
@@ -145,5 +177,17 @@ def home():
 def extract():
     return extract_pdf_data()
 
+
+@app.route('/adviesbox', methods=['GET', 'POST'])
+def adviesbox():
+    return gpt()
+
+
+def main():
+    load_dotenv()
+    
+    gpt()
+
 if __name__ == '__main__':
     app.run(debug=True)
+    # main()
