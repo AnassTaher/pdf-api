d641b0abd9e86fe4aefb62a1523ed79cbf71805d
Anafo <AnassTaher2003@hotmail.com>
2024-09-04 00:06:11 +0200

    fast commit

--- flask_app.py
+++ flask_app.py
@@ -1,34 +1,26 @@
-from PIL import Image, ImageDraw
+from PIL import Image
 from pdf2image import convert_from_path
 import json
 import os
 
-from pathlib import Path
-
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 
 import base64
-from dotenv import load_dotenv
-
-from openai import OpenAI
-
-
-from langchain_community.document_loaders import PyPDFLoader
-
+from pypdf import PdfReader
 
 
 app = Flask(__name__)
 CORS(app)
 
-
-
 def extract_pdf_data():
     
 
+    is_local = False
     if request.data:
         # print("Request data: ", request.data)
         data = request.get_json()
+        is_local = data.get("is_local", False)
         # access the "is_local" key
         if "pdf_data" in data:
             # pdf_data is in base64 format, decode it and save it to a file
@@ -48,18 +40,24 @@ def extract_pdf_data():
     current_dir = "mysite/"
     filename = "output"
 
+    
+
+    if is_local:
+        current_dir = ""
+
     pages = None
     try:
         pages = convert_from_path(f"{current_dir}{filename}.pdf", 500)
     except Exception as e:
         info_obj = {
             "error": str(e),
-            "mysite_files" : os.listdir("mysite/"),
-            "root_files" : os.listdir("/"),
+            "mysite_files" : os.listdir("mysite/") if os.path.exists("mysite/") else [],
+            "root_files" : os.listdir("."),
         }
 
         return json.dumps(info_obj, indent=4)
-    image_file_path = f"{filename}.jpg"
+    
+    image_file_path = f"{filename}_2.jpg"
     for count, page in enumerate(pages):
         if count == 1:
             page.save(image_file_path, 'JPEG')
@@ -76,7 +74,7 @@ def extract_pdf_data():
     cell_width = cropped_im.size[0] // (columns)
     cell_height = cropped_im.size[1] // (rows)
 
-    grid = [ [0]*columns for _ in range(rows) ]
+    grid = [ [0] * columns for _ in range(rows) ]
 
     # # Loop through each cell, check for each pixel if there is a color value < 100, if so mark it as 1
     #
@@ -96,11 +94,14 @@ def extract_pdf_data():
                         count = 1
             grid[i][j] = count
 
-    risico_data = scan_risico("output")
-
+    risico_data = scan_risico(filename, im)
+    is_bruto = scan_is_bruto(filename, pages)
+    woonsituatie = gpt()
     dict_info = {
         "data" : grid,
-        "risico" : risico_data
+        "risico" : risico_data,
+        "is_bruto" : is_bruto,
+        "woonsituatie" : woonsituatie
     }
 
     print("Grid: ")	
@@ -118,104 +119,14 @@ def extract_pdf_data():
     if request.data:
         os.remove("output.pdf")
 
+    print("woonsituatie: ", woonsituatie)
+    print("is_bruto: ", is_bruto)
+
     return json.dumps(dict_info)
 
+def scan_risico(filename: str, im: Image):
 
-def gpt():
-
-    project_folder = os.path.expanduser('~/mysite')  # adjust as appropriate
-    load_dotenv(os.path.join(project_folder, '.env'))
-    # load_dotenv()
-
-    client = OpenAI(
-        api_key=os.getenv("API_KEY")
-    )
-
-    try:
-
-        if request.data:
-            # print("Request data: ", request.data)
-            data = request.get_json()
-            # access the "is_local" key
-            if "pdf_data" in data:
-                # pdf_data is in base64 format, decode it and save it to a file
-                pdf_data = data["pdf_data"]
-
-
-                if pdf_data.startswith("data:application/pdf;base64,"):
-                    pdf_data = pdf_data[len("data:application/pdf;base64,"):]
-
-                # Decode the base64 data
-                decoded_pdf_data = base64.b64decode(pdf_data)
-
-                # Save the decoded data to a PDF file
-                with open("adviesbox.pdf", "wb") as pdf_file:
-                    pdf_file.write(decoded_pdf_data)
-    except Exception as e:
-        return "Error saving the pdf file"
-
-    pdf = "adviesbox.pdf"
-
-    loader = None
-    try:
-        loader = PyPDFLoader(pdf)
-    except Exception as e:
-        return "Error loading the pdf file"
-    
-    documents = None
-    try:
-        documents = loader.load()
-    except Exception as e:
-        return "Error loading the documents"
-
-
-    question = """
-    In het document, is het een huurwoning? Zo ja, wat is de huurprijs per maand. Zo niet, beantwoord deze vragen met behulp van sectie 2.1 in het document: Is de woning bedoeld voor blijven te wonen(primaire woning) of verkoop of verhuur? Is de woning verkocht? Wat is de verwachte verkoopwaarde(marktwaarde)? Wat zijn de verkoopkosten? Wat is de WOZ-waarde? Geef een json object terug met de volgende template:{ "type": string ("eigen", "huur", "inwonend"), "huurprijs" : int, "doel_woning": string, ("wonen", "verkoop", "verhuur") "is_verkocht": Boolean, "verkoopwaarde": int, "verkoopkosten": int, "woz" : int }. Gebruik geen text of iets, het enige wat ik wil krijgen is het json object, zonder apostrophe erbij(`). Dit zal worden gebruiktt in een API request dus ik moet het direct kunnen gebruiken als json object in python
-    """
-
-    # using the pdf's content in documents, and the question, use gpt-4o to answer the question
-
-    # make documents a string
-
-
-    doc_strings = [x.dict()['page_content'] for x in documents]
-    
-    pdf_string = " ".join(doc_strings)
-
-
-
-    answer = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {
-                "role": "user",
-                "content": question + " Here is the content of the pdf: " + pdf_string
-            }
-        ]
-    )
-
-    print(answer.choices[0].message.content)
-    woningsituatie = json.loads(answer.choices[0].message.content)
-    print(woningsituatie)
-
-    return woningsituatie
-
-
-def scan_risico(filename: str):
-
-    filename = f"{filename}"
-
-    pages = convert_from_path(f"{filename}.pdf", 500)
-
-    image_file_path = f"{filename}_2.jpg"
-    for count, page in enumerate(pages):
-        if count == 2:
-            page.save(image_file_path, 'JPEG')
-
-    im = Image.open(image_file_path)
-
-    crop_rectangle = (3280, 2880, 3890, 3900)
+    crop_rectangle = (3280, 3300, 3890, 4320)
     cropped_im = im.crop(crop_rectangle)
 
     rows = 6
@@ -249,9 +160,88 @@ def scan_risico(filename: str):
                         count = 1
             grid[i][j] = count
 
+    return grid
+
+def scan_is_bruto(filename: str, pages : list):
+
+    image_file_path = f"{filename}_1.jpg"
+    for count, page in enumerate(pages):
+        if count == 0:
+            page.save(image_file_path, 'JPEG')
+
+    im = Image.open(image_file_path)
+
+    # left, top, right, bottom
+    crop_rectangle = (200, 5290, 280, 5460)
+    cropped_im = im.crop(crop_rectangle)
+
+    height = cropped_im.size[1]
+
+    box_length = 50
+    offset = 15
+
+    cropped_im_box_1 = cropped_im.crop( (offset, offset, box_length + offset, box_length + offset) )
+    cropped_im_box_2 = cropped_im.crop(  (offset, 110, box_length + offset, 110 + box_length - offset) )
+
+
+    grid = [0, 0]
+
+    for x in range(box_length):
+        for y in range(box_length):
+            pixel = cropped_im_box_1.getpixel((x, y))
+            # pixel returns a RGB tuple (r, g, b)
+            is_black = True
+            for p in pixel:
+                if p > 0:
+                    is_black = False
+                    break
+            if is_black:
+                grid[0] = 1
+
+    for x in range(box_length):
+        for y in range(box_length - offset):
+            pixel = cropped_im_box_2.getpixel((x, y))
+            # pixel returns a RGB tuple (r, g, b)
+            is_black = True
+            for p in pixel:
+                if p > 0:
+                    is_black = False
+                    break
+            if is_black:
+                grid[1] = 1
+
+
     os.remove(image_file_path)
 
-    return grid
+    is_bruto = grid[0]
+    is_netto = grid[1]
+
+    print("Bruto netto grid: ")
+    for i in range(len(grid)):
+        print(grid[i])
+
+    if is_bruto:
+        return True
+    return False
+
+
+def gpt():
+
+    pdf = "output.pdf"
+    reader = PdfReader(pdf)
+    fields = reader.get_form_text_fields()
+
+    woonsituatie = {
+        "totaal_inkomen" : fields.get("Totaal maandelijks inkomen") or 0,
+        "huurlasten" : fields.get("Hyptoheek/Huurlasten") or 0,
+        "kosten_levensonderhoud" : fields.get("Kosten voor levensonderhoud") or 0,
+        "max_maandlast" : fields.get("Maximale acceptable maandlast") or 0,
+    }
+
+    for k, v in woonsituatie.items():
+        woonsituatie[k] = int(v)
+
+    return woonsituatie
 
 @app.route('/')
 def home():
@@ -262,17 +252,7 @@ def extract():
     return extract_pdf_data()
 
 
-@app.route('/adviesbox', methods=['GET', 'POST'])
-def adviesbox():
-    return gpt()
-
-
-def main():
-    load_dotenv()
-    
-    gpt()
 
 if __name__ == '__main__':
     app.run(debug=True)
     # main()
-    # scan_risico("doc2")
